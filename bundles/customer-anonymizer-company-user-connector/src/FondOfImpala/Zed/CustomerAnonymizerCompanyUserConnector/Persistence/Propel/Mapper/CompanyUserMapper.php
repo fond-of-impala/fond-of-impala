9ccd82bcb23c1257f17cff602c0ac7437767b35a
Julian Hyatt <dev@jzit.de>
2023-07-19 13:35:02 +0200

    🐛 fix entity usage instead of entity transfer usage

--- bundles/customer-anonymizer-company-user-connector/src/FondOfImpala/Zed/CustomerAnonymizerCompanyUserConnector/Persistence/Propel/Mapper/CompanyUserMapper.php
+++ bundles/customer-anonymizer-company-user-connector/src/FondOfImpala/Zed/CustomerAnonymizerCompanyUserConnector/Persistence/Propel/Mapper/CompanyUserMapper.php
@@ -6,12 +6,12 @@ use Generated\Shared\Transfer\CompanyTransfer;
 use Generated\Shared\Transfer\CompanyUserCollectionTransfer;
 use Generated\Shared\Transfer\CompanyUserTransfer;
 use Generated\Shared\Transfer\CustomerTransfer;
-use Generated\Shared\Transfer\SpyCompanyUserEntityTransfer;
+use Orm\Zed\CompanyUser\Persistence\SpyCompanyUser;
 
 class CompanyUserMapper implements CompanyUserMapperInterface
 {
     /**
-     * @param array<\Generated\Shared\Transfer\SpyCompanyUserEntityTransfer> $collection
+     * @param array<\Orm\Zed\CompanyUser\Persistence\SpyCompanyUser> $collection
      *
      * @return \Generated\Shared\Transfer\CompanyUserCollectionTransfer
      */
@@ -19,34 +19,33 @@ class CompanyUserMapper implements CompanyUserMapperInterface
     {
         $companyUserCollectionTransfer = new CompanyUserCollectionTransfer();
 
-        foreach ($collection as $companyUserEntityTransfer) {
-            $companyUserCollectionTransfer->addCompanyUser($this->mapEntityTransferToCompanyUserTransfer($companyUserEntityTransfer));
+        foreach ($collection as $companyUserEntity) {
+            $companyUserCollectionTransfer->addCompanyUser($this->mapEntityToCompanyUserTransfer($companyUserEntity));
         }
 
         return $companyUserCollectionTransfer;
     }
 
     /**
-     * @param \Generated\Shared\Transfer\SpyCompanyUserEntityTransfer $companyUserEntityTransfer
-     *
+     * @param \Orm\Zed\CompanyUser\Persistence\SpyCompanyUser $companyUserEntity
      * @return \Generated\Shared\Transfer\CompanyUserTransfer
      */
-    public function mapEntityTransferToCompanyUserTransfer(
-        SpyCompanyUserEntityTransfer $companyUserEntityTransfer
+    public function mapEntityToCompanyUserTransfer(
+        SpyCompanyUser $companyUserEntity
     ): CompanyUserTransfer {
-        $companyUserTransfer = (new CompanyUserTransfer())->fromArray($companyUserEntityTransfer->modifiedToArray(), true);
+        $companyUserTransfer = (new CompanyUserTransfer())->fromArray($companyUserEntity->toArray(), true);
 
-        if ($companyUserEntityTransfer->getCustomer()) {
+        if ($companyUserEntity->getCustomer()) {
             $customerTransfer = (new CustomerTransfer())->fromArray(
-                $companyUserEntityTransfer->getCustomer()->modifiedToArray(),
+                $companyUserEntity->getCustomer()->toArray(),
                 true,
             );
             $companyUserTransfer->setCustomer($customerTransfer);
         }
 
-        if ($companyUserEntityTransfer->getCompany()) {
+        if ($companyUserEntity->getCompany()) {
             $companyTransfer = (new CompanyTransfer())->fromArray(
-                $companyUserEntityTransfer->getCompany()->modifiedToArray(),
+                $companyUserEntity->getCompany()->toArray(),
                 true,
             );
             $companyUserTransfer->setCompany($companyTransfer);
