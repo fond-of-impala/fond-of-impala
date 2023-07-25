8e5a854ac25977b8200b0db1ccf99e40f316773a
Julian Hyatt <54834203+julianzimmermann@users.noreply.github.com>
2023-07-25 12:33:32 +0200

    📦 migrate company-type-converter from fond-of-spryker (#70)

---
+++ bundles/company-type-converter/src/FondOfImpala/Zed/CompanyTypeConverter/Business/CompanyTypeConverterBusinessFactory.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace FondOfImpala\Zed\CompanyTypeConverter\Business;
+
+use FondOfImpala\Zed\CompanyTypeConverter\Business\Model\CompanyReader;
+use FondOfImpala\Zed\CompanyTypeConverter\Business\Model\CompanyReaderInterface;
+use FondOfImpala\Zed\CompanyTypeConverter\Business\Model\CompanyTypeConverter;
+use FondOfImpala\Zed\CompanyTypeConverter\Business\Model\CompanyTypeConverterInterface;
+use FondOfImpala\Zed\CompanyTypeConverter\Business\Model\CompanyTypeConverterPluginExecutor;
+use FondOfImpala\Zed\CompanyTypeConverter\Business\Model\CompanyTypeConverterPluginExecutorInterface;
+use FondOfImpala\Zed\CompanyTypeConverter\Business\Model\CompanyTypeRoleWriter;
+use FondOfImpala\Zed\CompanyTypeConverter\Business\Model\CompanyTypeRoleWriterInterface;
+use FondOfImpala\Zed\CompanyTypeConverter\CompanyTypeConverterDependencyProvider;
+use FondOfImpala\Zed\CompanyTypeConverter\Dependency\Facade\CompanyTypeConverterToCompanyFacadeInterface;
+use FondOfImpala\Zed\CompanyTypeConverter\Dependency\Facade\CompanyTypeConverterToCompanyRoleFacadeInterface;
+use FondOfImpala\Zed\CompanyTypeConverter\Dependency\Facade\CompanyTypeConverterToCompanyTypeFacadeInterface;
+use FondOfImpala\Zed\CompanyTypeConverter\Dependency\Facade\CompanyTypeConverterToCompanyTypeRoleFacadeInterface;
+use FondOfImpala\Zed\CompanyTypeConverter\Dependency\Facade\CompanyTypeConverterToCompanyUserFacadeInterface;
+use FondOfImpala\Zed\CompanyTypeConverter\Dependency\Facade\CompanyTypeConverterToPermissionFacadeInterface;
+use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
+
+/**
+ * @method \FondOfImpala\Zed\CompanyTypeConverter\CompanyTypeConverterConfig getConfig()
+ */
+class CompanyTypeConverterBusinessFactory extends AbstractBusinessFactory
+{
+    /**
+     * @return \FondOfImpala\Zed\CompanyTypeConverter\Business\Model\CompanyTypeConverterInterface
+     */
+    public function createCompanyTypeConverter(): CompanyTypeConverterInterface
+    {
+        return new CompanyTypeConverter(
+            $this->getCompanyTypeFacade(),
+            $this->getCompanyRoleFacade(),
+            $this->getCompanyUserFacade(),
+            $this->createCompanyTypeRoleWriter(),
+            $this->getConfig(),
+            $this->createPluginExecutor(),
+        );
+    }
+
+    /**
+     * @return \FondOfImpala\Zed\CompanyTypeConverter\Business\Model\CompanyTypeRoleWriterInterface
+     */
+    protected function createCompanyTypeRoleWriter(): CompanyTypeRoleWriterInterface
+    {
+        return new CompanyTypeRoleWriter(
+            $this->getCompanyRoleFacade(),
+            $this->getCompanyTypeFacade(),
+            $this->getCompanyTypeRoleFacade(),
+            $this->getPermissionFacade(),
+            $this->getConfig(),
+        );
+    }
+
+    /**
+     * @return \FondOfImpala\Zed\CompanyTypeConverter\Business\Model\CompanyReaderInterface
+     */
+    public function createCompanyReader(): CompanyReaderInterface
+    {
+        return new CompanyReader(
+            $this->getCompanyFacade(),
+        );
+    }
+
+    /**
+     * @return \FondOfImpala\Zed\CompanyTypeConverter\Business\Model\CompanyTypeConverterPluginExecutorInterface
+     */
+    protected function createPluginExecutor(): CompanyTypeConverterPluginExecutorInterface
+    {
+        return new CompanyTypeConverterPluginExecutor(
+            $this->getCompanyTypeConverterPreSavePlugins(),
+            $this->getCompanyTypeConverterPostSavePlugins(),
+        );
+    }
+
+    /**
+     * @return \FondOfSpryker\Zed\CompanyTypeConverterExtension\Dependency\Plugin\CompanyTypeConverterPreSavePluginInterface[]
+     */
+    protected function getCompanyTypeConverterPreSavePlugins(): array
+    {
+        return $this->getProvidedDependency(CompanyTypeConverterDependencyProvider::COMPANY_TYPE_CONVERTER_PRE_SAVE_PLUGINS);
+    }
+
+    /**
+     * @return \FondOfSpryker\Zed\CompanyTypeConverterExtension\Dependency\Plugin\CompanyTypeConverterPostSavePluginInterface[]
+     */
+    protected function getCompanyTypeConverterPostSavePlugins(): array
+    {
+        return $this->getProvidedDependency(CompanyTypeConverterDependencyProvider::COMPANY_TYPE_CONVERTER_POST_SAVE_PLUGINS);
+    }
+
+    /**
+     * @return \FondOfImpala\Zed\CompanyTypeConverter\Dependency\Facade\CompanyTypeConverterToCompanyFacadeInterface
+     */
+    protected function getCompanyFacade(): CompanyTypeConverterToCompanyFacadeInterface
+    {
+        return $this->getProvidedDependency(CompanyTypeConverterDependencyProvider::FACADE_COMPANY);
+    }
+
+    /**
+     * @return \FondOfImpala\Zed\CompanyTypeConverter\Dependency\Facade\CompanyTypeConverterToPermissionFacadeInterface
+     */
+    protected function getPermissionFacade(): CompanyTypeConverterToPermissionFacadeInterface
+    {
+        return $this->getProvidedDependency(CompanyTypeConverterDependencyProvider::FACADE_PERMISSION);
+    }
+
+    /**
+     * @return \FondOfImpala\Zed\CompanyTypeConverter\Dependency\Facade\CompanyTypeConverterToCompanyRoleFacadeInterface
+     */
+    protected function getCompanyRoleFacade(): CompanyTypeConverterToCompanyRoleFacadeInterface
+    {
+        return $this->getProvidedDependency(CompanyTypeConverterDependencyProvider::FACADE_COMPANY_ROLE);
+    }
+
+    /**
+     * @return \FondOfImpala\Zed\CompanyTypeConverter\Dependency\Facade\CompanyTypeConverterToCompanyUserFacadeInterface
+     */
+    protected function getCompanyUserFacade(): CompanyTypeConverterToCompanyUserFacadeInterface
+    {
+        return $this->getProvidedDependency(CompanyTypeConverterDependencyProvider::FACADE_COMPANY_USER);
+    }
+
+    /**
+     * @return \FondOfImpala\Zed\CompanyTypeConverter\Dependency\Facade\CompanyTypeConverterToCompanyTypeFacadeInterface
+     */
+    protected function getCompanyTypeFacade(): CompanyTypeConverterToCompanyTypeFacadeInterface
+    {
+        return $this->getProvidedDependency(CompanyTypeConverterDependencyProvider::FACADE_COMPANY_TYPE);
+    }
+
+    /**
+     * @return \FondOfImpala\Zed\CompanyTypeConverter\Dependency\Facade\CompanyTypeConverterToCompanyTypeRoleFacadeInterface
+     */
+    protected function getCompanyTypeRoleFacade(): CompanyTypeConverterToCompanyTypeRoleFacadeInterface
+    {
+        return $this->getProvidedDependency(CompanyTypeConverterDependencyProvider::FACADE_COMPANY_TYPE_ROLE);
+    }
+}
---
+++ bundles/company-type-converter/src/FondOfImpala/Zed/CompanyTypeConverter/Business/Exception/CompanyRoleCouldNotBeDeletedException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FondOfImpala\Zed\CompanyTypeConverter\Business\Exception;
+
+use Exception;
+
+class CompanyRoleCouldNotBeDeletedException extends Exception
+{
+}
