99a0172d8272ca5013a735cfef95c7bc28808092
Daniel Rose <daniel-rose@gmx.de>
2023-10-24 09:46:43 +0200

    ✨ Make not allowed filter field types configurable - add config class to define not allowed filter field types - handle configurable not allowed filter field types in customer filter - prepare unit tests for new logic - ...

--- bundles/company-cart-search-rest-api/src/FondOfImpala/Zed/CompanyCartSearchRestApi/Communication/Plugin/CartSearchRestApiExtension/CustomerSearchQuoteQueryExpanderPlugin.php
+++ bundles/company-cart-search-rest-api/src/FondOfImpala/Zed/CompanyCartSearchRestApi/Communication/Plugin/CartSearchRestApiExtension/CustomerSearchQuoteQueryExpanderPlugin.php
@@ -18,6 +18,7 @@ use Propel\Runtime\ActiveQuery\Criteria;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
 /**
+ * @method \FondOfImpala\Zed\CompanyCartSearchRestApi\CompanyCartSearchRestApiConfig getConfig()
  * @method \FondOfImpala\Zed\CompanyCartSearchRestApi\Persistence\CompanyCartSearchRestApiRepositoryInterface getRepository()
  */
 class CustomerSearchQuoteQueryExpanderPlugin extends AbstractPlugin implements SearchQuoteQueryExpanderPluginInterface
@@ -28,11 +29,6 @@ class CustomerSearchQuoteQueryExpanderPlugin extends AbstractPlugin implements S
     protected const REQUIRED_FILTER_FIELD_TYPE = CompanyCartSearchRestApiConstants::FILTER_FIELD_TYPE_ID_CUSTOMER;
 
     /**
-     * @var string
-     */
-    protected const NOT_ALLOWED_FILTER_FIELD_TYPE = CompanyCartSearchRestApiConstants::FILTER_FIELD_TYPE_COMPANY_UUID;
-
-    /**
      * @param array<\Generated\Shared\Transfer\FilterFieldTransfer> $filterFieldTransfers
      *
      * @return bool
@@ -40,9 +36,10 @@ class CustomerSearchQuoteQueryExpanderPlugin extends AbstractPlugin implements S
     public function isApplicable(array $filterFieldTransfers): bool
     {
         $isApplicable = false;
+        $notAllowedFilterFieldTypes = $this->getConfig()->getNotAllowedFilterFieldTypesForCustomerFilter();
 
         foreach ($filterFieldTransfers as $filterFieldTransfer) {
-            if ($filterFieldTransfer->getType() === static::NOT_ALLOWED_FILTER_FIELD_TYPE) {
+            if (in_array($filterFieldTransfer->getType(), $notAllowedFilterFieldTypes, true)) {
                 return false;
             }
 
---
+++ bundles/company-cart-search-rest-api/src/FondOfImpala/Zed/CompanyCartSearchRestApi/CompanyCartSearchRestApiConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace FondOfImpala\Zed\CompanyCartSearchRestApi;
+
+use FondOfImpala\Shared\CompanyCartSearchRestApi\CompanyCartSearchRestApiConstants;
+use Spryker\Zed\Kernel\AbstractBundleConfig;
+
+/**
+ * @codeCoverageIgnore
+ */
+class CompanyCartSearchRestApiConfig extends AbstractBundleConfig
+{
+    /**
+     * @return array<string>
+     */
+    public function getNotAllowedFilterFieldTypesForCustomerFilter(): array {
+        return [
+            CompanyCartSearchRestApiConstants::FILTER_FIELD_TYPE_COMPANY_UUID
+        ];
+    }
+}
