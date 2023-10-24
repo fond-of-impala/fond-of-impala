9760384d050c281fe2c131c9bb0820d09ac67570
Julian Hyatt <dev@jzit.de>
2023-10-24 16:27:23 +0200

    🧹 extract hard dependency introduce new bundle

---
+++ bundles/company-users-rest-api-price-list-connector/README.md
@@ -0,0 +1,9 @@
+# Company Users Rest Api Price List Connector Module
+[![CI](https://github.com/fond-of-impala/company-users-rest-api-price-list-connector/actions/workflows/main.yml/badge.svg)](https://github.com/fond-of-impala/company-users-rest-api-price-list-connector/actions/workflows/main.yml)
+[![license](https://img.shields.io/github/license/fond-of-impala/company-users-rest-api-price-list-connector.svg)](https://packagist.org/packages/fond-of-impala/company-users-rest-api-price-list-connector)
+
+## Installation
+
+```
+composer require fond-of-impala/company-users-rest-api-price-list-connector
+```
---
+++ bundles/company-users-rest-api/src/FondOfImpala/Zed/CompanyUsersRestApi/Persistence/Expander/CompanyUserQueryExpander.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FondOfImpala\Zed\CompanyUsersRestApi\Persistence\Expander;
+
+use Orm\Zed\CompanyUser\Persistence\SpyCompanyUserQuery;
+
+class CompanyUserQueryExpander implements CompanyUserQueryExpanderInterface
+{
+    /**
+     * @var array|\FondOfOryx\Zed\CompanyUsersRestApiExtension\Dependency\Plugin\QueryExpander\CompanyUserQueryExpanderPluginInterface[]
+     */
+    protected array $expanderPlugins;
+
+    /**
+     * @param array<\FondOfOryx\Zed\CompanyUsersRestApiExtension\Dependency\Plugin\QueryExpander\CompanyUserQueryExpanderPluginInterface> $expanderPlugins
+     */
+    public function __construct(array $expanderPlugins)
+    {
+        $this->expanderPlugins = $expanderPlugins;
+    }
+
+    /**
+     * @param \Orm\Zed\CompanyUser\Persistence\SpyCompanyUserQuery $companyUserQuery
+     * @return \Orm\Zed\CompanyUser\Persistence\SpyCompanyUserQuery
+     */
+    public function expand(SpyCompanyUserQuery $companyUserQuery): SpyCompanyUserQuery
+    {
+        foreach ($this->expanderPlugins as $expanderPlugin) {
+            $companyUserQuery = $expanderPlugin->expandCompanyUserQuery($companyUserQuery);
+        }
+
+        return $companyUserQuery;
+    }
+}
