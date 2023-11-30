197d6bc8d2e446cd3cb874ff90d75a0cf6e932c4
gengjozsef <gengjozsef86@gmail.com>
2023-11-30 16:51:13 +0100

    WIP `ItemExpanderTest`

--- bundles/conditional-availability-cart-connector/tests/FondOfImpala/Zed/ConditionalAvailabilityCartConnector/Business/Expander/ItemExpanderTest.php
+++ bundles/conditional-availability-cart-connector/tests/FondOfImpala/Zed/ConditionalAvailabilityCartConnector/Business/Expander/ItemExpanderTest.php
@@ -2,7 +2,9 @@
 
 namespace FondOfImpala\Zed\ConditionalAvailabilityCartConnector\Business\Expander;
 
+use ArrayObject;
 use Codeception\Test\Unit;
+use FondOfImpala\Shared\ConditionalAvailability\ConditionalAvailabilityConstants;
 use FondOfImpala\Zed\ConditionalAvailabilityCartConnector\Business\Filter\ConditionalAvailabilityPeriodsFilterInterface;
 use FondOfImpala\Zed\ConditionalAvailabilityCartConnector\Business\Finder\IndexFinderInterface;
 use FondOfImpala\Zed\ConditionalAvailabilityCartConnector\Business\Generator\DeliveryDateGeneratorInterface;
@@ -15,18 +17,49 @@ use PHPUnit\Framework\MockObject\MockObject;
 
 class ItemExpanderTest extends Unit
 {
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\FondOfImpala\Zed\ConditionalAvailabilityCartConnector\Business\Filter\ConditionalAvailabilityPeriodsFilterInterface
+     */
     protected MockObject|ConditionalAvailabilityPeriodsFilterInterface $conditionalAvailabilityPeriodsFilterMock;
 
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\Generated\Shared\Transfer\ConditionalAvailabilityPeriodTransfer
+     */
+    protected MockObject|ConditionalAvailabilityPeriodTransfer $conditionalAvailabilityPeriodMock;
+
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\FondOfImpala\Zed\ConditionalAvailabilityCartConnector\Business\Finder\IndexFinderInterface
+     */
     protected IndexFinderInterface|MockObject $indexFinderMock;
 
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\FondOfImpala\Zed\ConditionalAvailabilityCartConnector\Business\Generator\MessageGeneratorInterface
+     */
     protected MessageGeneratorInterface|MockObject $messageGeneratorMock;
 
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\FondOfImpala\Zed\ConditionalAvailabilityCartConnector\Business\Generator\DeliveryDateGeneratorInterface
+     */
     protected DeliveryDateGeneratorInterface|MockObject $deliveryDateGeneratorMock;
 
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\FondOfImpala\Zed\ConditionalAvailabilityCartConnector\Business\Reducer\ConditionalAvailabilityPeriodsReducerInterface
+     */
     protected ConditionalAvailabilityPeriodsReducerInterface|MockObject $conditionalAvailabilityPeriodsReducerMock;
 
+    /**
+     * @var \FondOfImpala\Zed\ConditionalAvailabilityCartConnector\Business\Expander\ItemExpanderInterface
+     */
+    protected ItemExpanderInterface $expander;
+
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\Generated\Shared\Transfer\ItemTransfer
+     */
     protected MockObject|ItemTransfer $itemTransferMock;
 
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\Generated\Shared\Transfer\MessageTransfer
+     */
     protected MockObject|MessageTransfer $messageTransferMock;
 
     /**
@@ -37,6 +70,10 @@ class ItemExpanderTest extends Unit
         parent::_before();
 
         $this->conditionalAvailabilityPeriodsFilterMock = $this->getMockBuilder(ConditionalAvailabilityPeriodsFilterInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->conditionalAvailabilityPeriodMock = $this->getMockBuilder(ConditionalAvailabilityPeriodTransfer::class)
             ->disableOriginalConstructor()
             ->getMock();
 
@@ -71,7 +108,7 @@ class ItemExpanderTest extends Unit
             $this->getMockBuilder(ConditionalAvailabilityPeriodTransfer::class)
                 ->disableOriginalConstructor()
                 ->getMock()
-        ]
+        ];
 
         $this->expander = new ItemExpander(
             $this->conditionalAvailabilityPeriodsFilterMock,
@@ -79,6 +116,41 @@ class ItemExpanderTest extends Unit
             $this->messageGeneratorMock,
             $this->deliveryDateGeneratorMock,
             $this->conditionalAvailabilityPeriodsReducerMock
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testExpand(): void
+    {
+        $groupedConditionalAvailabilities = new ArrayObject();
+        $conditionalAvailabilityPeriodTransfers = new ArrayObject();
+        $conditionalAvailabilityPeriodTransfers->append($this->conditionalAvailabilityPeriodMock);
+        $index = 1;
+
+        $this->itemTransferMock->expects(static::atLeastOnce())
+            ->method('getDeliveryDate')
+            ->willReturn(ConditionalAvailabilityConstants::KEY_EARLIEST_DATE);
+
+        $this->conditionalAvailabilityPeriodsFilterMock->expects(static::atLeastOnce())
+            ->method('filterFromGroupedConditionalAvailabilitiesByItem')
+            ->with($groupedConditionalAvailabilities, $this->itemTransferMock)
+            ->willReturn($conditionalAvailabilityPeriodTransfers);
+
+        $this->indexFinderMock->expects(static::atLeastOnce())
+            ->method('findEarliestFromConditionalAvailabilityPeriods')
+            ->with($conditionalAvailabilityPeriodTransfers, $this->itemTransferMock)
+            ->willReturn($index);
+
+        $this->conditionalAvailabilityPeriodsReducerMock->expects(static::atLeastOnce())
+            ->method('reduceByItemAndEffectedIndex')
+            ->with($conditionalAvailabilityPeriodTransfers, $this->itemTransferMock, $index)
+            ->willReturn();
+
+        $this->expander->expand(
+            $this->itemTransferMock,
+            $groupedConditionalAvailabilities
         );
     }
 
