d984494f07d92cf6903d221db499c4ba0489f10c
Julian Hyatt <dev@jzit.de>
2023-12-19 15:17:03 +0100

    🧹 skip incomplete bulk item instead of skip all

--- bundles/company-users-bulk-rest-api/src/FondOfImpala/Zed/CompanyUsersBulkRestApi/Business/Manager/BulkManager.php
+++ bundles/company-users-bulk-rest-api/src/FondOfImpala/Zed/CompanyUsersBulkRestApi/Business/Manager/BulkManager.php
@@ -62,13 +62,14 @@ class BulkManager implements BulkManagerInterface
      * @param \Psr\Log\LoggerInterface $logger
      */
     public function __construct(
-        PermissionCheckerInterface $permissionChecker,
-        CompanyUsersBulkRestApiToEventFacadeInterface $eventFacade,
+        PermissionCheckerInterface                          $permissionChecker,
+        CompanyUsersBulkRestApiToEventFacadeInterface       $eventFacade,
         CompanyUsersBulkRestApiToCompanyUserFacadeInterface $companyUserFacade,
-        BulkDataPluginExecutionerInterface $pluginExecutioner,
-        CompanyUsersBulkRestApiRepositoryInterface $repository,
-        LoggerInterface $logger
-    ) {
+        BulkDataPluginExecutionerInterface                  $pluginExecutioner,
+        CompanyUsersBulkRestApiRepositoryInterface          $repository,
+        LoggerInterface                                     $logger
+    )
+    {
         $this->permissionChecker = $permissionChecker;
         $this->eventFacade = $eventFacade;
         $this->companyUserFacade = $companyUserFacade;
@@ -84,7 +85,8 @@ class BulkManager implements BulkManagerInterface
      */
     public function handleBulkRequest(
         RestCompanyUsersBulkRequestTransfer $restCompanyUsersBulkRequestTransfer
-    ): RestCompanyUsersBulkResponseTransfer {
+    ): RestCompanyUsersBulkResponseTransfer
+    {
         try {
             $restCompanyUsersBulkRequestTransfer = $this->pluginExecutioner->executePreHandlePlugins($restCompanyUsersBulkRequestTransfer);
 
@@ -137,13 +139,13 @@ class BulkManager implements BulkManagerInterface
                 $roleCollection = (new CompanyRoleCollectionTransfer())->addRole($role);
                 foreach ($company->getCompanyBusinessUnits() as $companyBusinessUnit) {
                     $companyUserTransfer = $this->createDummyCompanyUserTransfer()
-                    ->setCustomerReference($customer->getCustomerReference())
-                    ->setFkCustomer($customer->getIdCustomer())
-                    ->setCompanyRoleCollection($roleCollection)
-                    ->setFkCompany($company->getIdCompany())
-                    ->setCompany((new CompanyTransfer())->fromArray($company->toArray(), true))
-                    ->setCustomer((new CustomerTransfer())->fromArray($customer->toArray(), true))
-                    ->setFkCompanyBusinessUnit($companyBusinessUnit->getIdCompanyBusinessUnit());
+                        ->setCustomerReference($customer->getCustomerReference())
+                        ->setFkCustomer($customer->getIdCustomer())
+                        ->setCompanyRoleCollection($roleCollection)
+                        ->setFkCompany($company->getIdCompany())
+                        ->setCompany((new CompanyTransfer())->fromArray($company->toArray(), true))
+                        ->setCustomer((new CustomerTransfer())->fromArray($customer->toArray(), true))
+                        ->setFkCompanyBusinessUnit($companyBusinessUnit->getIdCompanyBusinessUnit());
 
                     if ($this->repository->isCompanyUserAlreadyAvailable($companyUserTransfer) === true) {
                         continue;
@@ -202,15 +204,42 @@ class BulkManager implements BulkManagerInterface
      */
     protected function prepareData(
         RestCompanyUsersBulkItemCollectionTransfer $restCompanyUsersBulkItemCollectionTransfer
-    ): CompanyUsersBulkPreparationCollectionTransfer {
+    ): CompanyUsersBulkPreparationCollectionTransfer
+    {
         $collection = new CompanyUsersBulkPreparationCollectionTransfer();
 
         foreach ($restCompanyUsersBulkItemCollectionTransfer->getItems() as $item) {
+            $company = $item->getCompany();
+            $customer = $item->getCustomer();
+
+            if ($company === null || $customer === null) {
+                continue;
+            }
+
             $preparedItem = (new CompanyUsersBulkPreparationTransfer())->setItem($item);
             $collection->addItem($preparedItem);
         }
 
-        return $this->pluginExecutioner->executeExpand($collection);
+        return $this->incompleteDataCleanup($this->pluginExecutioner->executeExpand($collection));
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\CompanyUsersBulkPreparationCollectionTransfer $collection
+     * @return \Generated\Shared\Transfer\CompanyUsersBulkPreparationCollectionTransfer
+     */
+    protected function incompleteDataCleanup(CompanyUsersBulkPreparationCollectionTransfer $collection): CompanyUsersBulkPreparationCollectionTransfer
+    {
+        $cleanupCollection = new CompanyUsersBulkPreparationCollectionTransfer();
+
+        foreach ($collection->getItems() as $itemTransfer) {
+            if ($itemTransfer->getCompany() === null || $itemTransfer->getCustomer() === null) {
+                $this->logger->warning(sprintf('Customer or Company not found! Data: %s', json_encode($itemTransfer->getItem()->toArray())));
+                continue;
+            }
+            $cleanupCollection->addItem($itemTransfer);
+        }
+
+        return $cleanupCollection;
     }
 
     /**
@@ -245,9 +274,9 @@ class BulkManager implements BulkManagerInterface
      * @param \Generated\Shared\Transfer\CompanyUsersBulkCompanyTransfer $companyTransfer
      * @param string $role
      *
+     * @return \Generated\Shared\Transfer\CompanyRoleTransfer
      * @throws \Exception
      *
-     * @return \Generated\Shared\Transfer\CompanyRoleTransfer
      */
     protected function resolveRole(CompanyUsersBulkCompanyTransfer $companyTransfer, string $role): CompanyRoleTransfer
     {
