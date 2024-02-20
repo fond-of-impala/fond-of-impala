925bd37c7a9453d399327c7088e60c796af17238
Julian Hyatt <54834203+julianzimmermann@users.noreply.github.com>
2024-02-20 18:32:43 +0100

    add new bundle conditional availability company search rest api (#215)

---
+++ bundles/conditional-availability-company-search-rest-api/src/FondOfImpala/Glue/ConditionalAvailabilityCompanySearchRestApi/Communication/Plugin/CompanySearchRestApi/AvailabilityChannelFallbackRestCompanySearchResultItemExpanderPlugin.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace FondOfImpala\Glue\ConditionalAvailabilityCompanySearchRestApi\Communication\Plugin\CompanySearchRestApi;
+
+use FondOfOryx\Glue\CompanySearchRestApiExtension\Dependency\Plugin\RestCompanySearchResultItemExpanderPluginInterface;
+use Generated\Shared\Transfer\CompanyTransfer;
+use Generated\Shared\Transfer\RestCompanySearchResultItemTransfer;
+use Spryker\Glue\Kernel\AbstractPlugin;
+
+/**
+ * @method \FondOfImpala\Glue\ConditionalAvailabilityCompanySearchRestApi\ConditionAvailabilityCompanySearchRestApiConfig getConfig()
+ */
+class AvailabilityChannelFallbackRestCompanySearchResultItemExpanderPlugin extends AbstractPlugin implements RestCompanySearchResultItemExpanderPluginInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\RestCompanySearchResultItemTransfer $restCompanySearchResultItemTransfer
+     * @param \Generated\Shared\Transfer\CompanyTransfer $companyTransfer
+     *
+     * @return \Generated\Shared\Transfer\RestCompanySearchResultItemTransfer
+     */
+    public function expand(
+        RestCompanySearchResultItemTransfer $restCompanySearchResultItemTransfer,
+        CompanyTransfer $companyTransfer
+    ): RestCompanySearchResultItemTransfer {
+        $channel = $companyTransfer->getAvailabilityChannel();
+
+        if ($channel === null || $channel === '') {
+            $channel = $this->getConfig()->getFallbackAvailabilityChannel();
+        }
+
+        return $restCompanySearchResultItemTransfer->setAvailabilityChannel($channel);
+    }
+}
