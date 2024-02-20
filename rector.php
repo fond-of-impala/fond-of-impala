e922929941a1433c93540d32c25e1f524627b245
Daniel Rose <daniel-rose@gmx.de>
2024-02-20 14:40:36 +0100

    prepare rector

--- rector.php
+++ rector.php
@@ -14,12 +14,8 @@ use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/bundles/business-central-order-budgets-bulk-rest-api/src',
-        __DIR__ . '/bundles/business-central-order-budgets-bulk-rest-api/tests',
-        __DIR__ . '/bundles/order-budgets-bulk-rest-api/src',
-        __DIR__ . '/bundles/order-budgets-bulk-rest-api/tests',
-        __DIR__ . '/bundles/order-budgets-bulk-rest-api-extension/src',
-        __DIR__ . '/bundles/order-budgets-bulk-rest-api-extension/tests',
+        __DIR__ . '/bundles/*/src',
+        __DIR__ . '/bundles/*/tests',
     ]);
 
     $rectorConfig->sets([
