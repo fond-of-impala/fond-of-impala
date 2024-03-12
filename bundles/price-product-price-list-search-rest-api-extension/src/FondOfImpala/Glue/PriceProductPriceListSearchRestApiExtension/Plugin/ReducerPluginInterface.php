3f8d6a7af405ccb5d765db03a6b9117951d1b7f6
Julian Hyatt <dev@jzit.de>
2024-03-12 16:14:21 +0100

    wip

--- bundles/price-product-price-list-search-rest-api-extension/src/FondOfImpala/Glue/PriceProductPriceListSearchRestApiExtension/Plugin/ReducerPluginInterface.php
+++ bundles/price-product-price-list-search-rest-api-extension/src/FondOfImpala/Glue/PriceProductPriceListSearchRestApiExtension/Plugin/ReducerPluginInterface.php
@@ -2,13 +2,11 @@
 
 namespace FondOfImpala\Glue\PriceProductPriceListSearchRestApiExtension\Plugin;
 
-/**
- * @method \FondOfImpala\Glue\PriceProductPriceListSearchRestApiCompanyCurrencyConnector\PriceProductPriceListSearchRestApiCompanyCurrencyConnectorFactory getFactory()
- */
 interface ReducerPluginInterface
 {
     /**
      * @param array $data
+     *
      * @return array
      */
     public function reduce(array $data): array;
