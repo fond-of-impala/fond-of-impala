0cf9da4ea35df9f1e94901b5d2c0551376e84fa7
Julian Hyatt <dev@jzit.de>
2024-09-24 15:25:36 +0200

    wip

--- bundles/company-users-rest-api/src/FondOfImpala/Zed/CompanyUsersRestApi/Communication/CompanyUsersRestApiCommunicationFactory.php
+++ bundles/company-users-rest-api/src/FondOfImpala/Zed/CompanyUsersRestApi/Communication/CompanyUsersRestApiCommunicationFactory.php
@@ -8,7 +8,6 @@ use Spryker\Zed\Kernel\Communication\AbstractCommunicationFactory;
 
 /**
  * @method \FondOfImpala\Zed\CompanyUsersRestApi\CompanyUsersRestApiConfig getConfig()
- * @method \FondOfImpala\Zed\CompanyUsersRestApi\Persistence\CompanyUsersRestApiEntityManagerInterface getEntityManager()
  * @method \FondOfImpala\Zed\CompanyUsersRestApi\Persistence\CompanyUsersRestApiRepositoryInterface getRepository()
  * @method \FondOfImpala\Zed\CompanyUsersRestApi\Business\CompanyUsersRestApiFacadeInterface getFacade()
  */
